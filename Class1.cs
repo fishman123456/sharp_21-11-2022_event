c3d12936b036eb2a2991f6dac805c75de8f835b3
F_D <den9152@yandex.ru>
2022-11-21 20:50:41 +0700

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿class Count_
+{
+    public delegate void Count_one(int co);
+
+    public event Count_one Co_ev;
+
+
+    public void circle(int co)
+    {
+
+        for (int i = 0; i <= 1200; i++)
+        {
+            if (i == co)
+            {
+                Co_ev(i);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+Count_ f = new Count_();
+f.Co_ev += massage;
+f.circle(4);
+
+f.circle(5);
+f.circle(60);
+
+void  massage(int co)
+{
+    Console.WriteLine($"Сообщение{co} ");
+}
+
